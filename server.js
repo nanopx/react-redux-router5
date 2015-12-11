5d86d27d80a8ceedbad779aa94e4e5a3225f0fab
吉武篤司 <0nanopx@gmail.com>
2015-12-11 11:07:16 +0900

    fixed express server

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express();
 app.use(express.static(__dirname + '/static'));
 app.get('*', (req, res) => {
-  return res.sendFile('./static/index.html');
+  return res.sendFile(__dirname + '/static/index.html');
 });
 app.listen(3000, () => {
   console.info('Initialized production server'); // eslint-disable-line no-console
